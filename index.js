04d7e708bec2690ae418b832fb3f953150e23304
slowey <truonglevinhphuc2006@gmail.com>
2024-06-11 13:21:26 +0700

    restart backend server

--- index.js
+++ index.js
@@ -1,73 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
+const { initializeApp } = require("firebase-admin/app");
 
-// Middleware to parse incoming JSON
-app.use(express.json());
+initializeApp();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-// Load environment variables
-require("dotenv").config();
-
-// Connect to MongoDB
-const mongoURI = process.env.MONGO_URI; // Get MongoDB URI from environment variable
-mongoose
-    .connect(mongoURI)
-    .then(() => {
-        console.log("Connected to the database");
-        // Start the server after successful database connection
-        startServer();
-    })
-    .catch((error) => {
-        console.error("Error connecting to the database", error);
-        process.exit(1); // Exit the process with a non-zero code
-    });
-
-// Start the server
-const startServer = () => {
-    // Routes
-    app.get("/", (req, res) => {
-        res.send("Hello World!");
-    });
-
-    // Define user schema
-    const userSchema = new mongoose.Schema({
-        username: String,
-        password: String,
-        role: String,
-    });
-
-    // Create a User model with the schema
-    const User = mongoose.model("User", userSchema, "users");
-
-    app.get("/users", async (req, res) => {
-        try {
-            const users = await User.find();
-            res.status(200).json(users);
-        } catch (error) {
-            console.error("Error getting users", error);
-            res.status(500).json({ message: "Internal server error" });
-        }
-    });
-
-    app.post("/users", async (req, res) => {
-        try {
-            const { username, password, role } = req.body;
-            const user = new User({ username, password, role });
-            await user.save();
-            res.status(201).json({ message: "User added successfully" });
-        } catch (error) {
-            console.error("Error adding user", error);
-            res.status(500).json({ message: "Internal server error" });
-        }
-    });
-
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).json({ message: "Something broke!" });
-    });
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server started on port ${PORT}`);
-    });
-};
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
