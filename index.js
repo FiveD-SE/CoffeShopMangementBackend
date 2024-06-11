b2774e67c6a14de2a40a1a20f2e0b771ee5c138e
slowey <truonglevinhphuc2006@gmail.com>
2024-06-11 13:43:25 +0700

    init landing page

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const { initializeApp } = require("firebase-admin/app");
 
-initializeApp();
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
