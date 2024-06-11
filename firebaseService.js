00613135fbb7816c1413a26387ce8a31ffc4ec7e
slowey <truonglevinhphuc2006@gmail.com>
2024-06-11 15:54:26 +0700

    delete staff api update

---
+++ firebaseService.js
@@ -0,0 +1,23 @@
+const admin = require("firebase-admin");
+const { applicationDefault, cert } = require("firebase-admin/app");
+require("dotenv").config();
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
+admin.initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+module.exports = admin;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const admin = require("./firebaseService");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -8,6 +9,20 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.delete("/users/:uid", async (req, res) => {
+    try {
+        const uid = req.params.uid;
+        await admin.auth().deleteUser(uid);
+        await admin.firestore().collection("cashier").doc(uid).delete();
+        await admin.firestore().collection("users").doc(uid).delete();
+
+        console.log("Successfully deleted user");
+        res.sendStatus(200);
+    } catch (error) {
+        console.log("Error deleting user:", error);
+        res.status(500).send("Error deleting user");
+    }
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
